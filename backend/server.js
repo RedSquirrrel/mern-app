0d928dec61718b8914fe9c9c8cbd5b19d1c0c3fa
RedSquirrrel <anet1@rocketmail.com>
2022-03-08 16:32:51 +0000

    deploy

--- backend/server.js
+++ backend/server.js
@@ -25,6 +25,8 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../frontend/build')));
 
   app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')));
+} else {
+  app.get('/', (req, res) => res.send('Please set to production'));
 }
 
 // overwrite the default express error handler
